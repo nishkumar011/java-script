b1e45dcf42da76f224e50878d14e1fca9c2dea69
nishkumar011 <nishkumar011@gmail.com>
2025-11-29 10:32:35 +0000

    string work

---
+++ 01basics/05_strings.js
@@ -0,0 +1,37 @@
+// const gameName = "Nishant"
+const repoCount = 50
+
+//console.log(gameName + repoCount + "Value");
+
+//console.log(`hello my gameName is ${gameName} and my repo count is ${repoCount}`);
+
+const gameName = new String(`hitesh-hc-com`)
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`t`));
+
+
+const newstring = gameName.substring(0,4) // we cannot give negative indexes in substring method.
+console.log(newstring);
+
+const anotherstring = gameName.slice(-8,4) // we can give negative indexes in substring method.
+console.log(anotherstring);
+
+const newstringone = "   hitesh   "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+const url = "https://nishant.com/nishant%20kumar"
+
+console.log(url.replace('%20' , '-'));
+
+console.log(url.includes('nishant'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
