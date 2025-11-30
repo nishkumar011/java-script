870ab4d8595e8591320306ffa898a32c3874b429
nishkumar011 <nishkumar011@gmail.com>
2025-11-30 10:26:22 +0000

    math and random

--- largest_number.js
+++ largest_number.js
@@ -1,16 +1,37 @@
 const score = 400
-console.log(score)
+// console.log(score)
 
 const balance = new Number(1000)
-console.log(balance)
-console.log(balance.toString().length)
-console.log(typeof balance)
-console.log(typeof score)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(typeof balance)
+// console.log(typeof score)
 const marks = 90.9295
-console.log(typeof marks);
+// console.log(typeof marks);
 
-console.log(marks.toString().length)
-console.log(typeof marks);
+// console.log(marks.toString().length)
+// console.log(typeof marks);
 
-console.log(marks.toFixed(3))
-console.log(marks.toPrecision(1))
+// console.log(marks.toFixed(3))
+// console.log(marks.toPrecision(1))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(10,203,20,80,90));
+// console.log(Math.max(10,203,20,80,90));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.ceil(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
